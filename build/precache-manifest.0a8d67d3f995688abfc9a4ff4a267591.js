9cf77209e0520e0b651941cdf694979aef3c3c9e
givara <givoo25@gmail.com>
2020-06-11 14:08:31 +0200

    saving changes

---
+++ build/precache-manifest.0a8d67d3f995688abfc9a4ff4a267591.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb35c19c069bc38c1761d4c9998b072",
+    "url": "/alleppo-sweets/index.html"
+  },
+  {
+    "revision": "dae6af0c1445d9395630",
+    "url": "/alleppo-sweets/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fcc2321a1aaa9d93b62d",
+    "url": "/alleppo-sweets/static/css/main.47e9e992.chunk.css"
+  },
+  {
+    "revision": "dae6af0c1445d9395630",
+    "url": "/alleppo-sweets/static/js/2.0f05c4c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/alleppo-sweets/static/js/2.0f05c4c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc2321a1aaa9d93b62d",
+    "url": "/alleppo-sweets/static/js/main.c3639b4f.chunk.js"
+  },
+  {
+    "revision": "f8d097691254cffdf005",
+    "url": "/alleppo-sweets/static/js/runtime-main.93125de0.js"
+  },
+  {
+    "revision": "13f06f70d645a2b5d4a609929a828d6c",
+    "url": "/alleppo-sweets/static/media/salad.13f06f70.jpg"
+  }
+]);
